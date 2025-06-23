959bf5db348c0495a86ba9ab4d19d82637953318
stoffeu <kaikestoffell@gmail.com>
2025-06-23 19:10:43 -0300

    Delete apiCRUD-main directory

--- apiCRUD-main/crud-api/src/main/java/com/example/crud_api/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.crudapi.repository;
-
-import com.example.crudapi.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
